acba9e3b112155214399debd83ea271e07f82f01
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2021-11-10 14:43:10 -0300

    Folders for training and for the bot added

---
+++ renshuu/renshuu.py
@@ -0,0 +1,14 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk 
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+
+
