fe6e9dd82e9e7154fe32c657813b5b5d126d334f
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2021-11-10 17:24:42 -0300

    Update renshuu.py

--- renshuu/renshuu.py
+++ renshuu/renshuu.py
@@ -3,8 +3,8 @@ import json
 import pickle
 import numpy as np
 
-import nltk 
-from nltk.stem import WordNetLemmatizer
+#import nltk 
+#from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
