ab23189508a6f90d21784d651e2c1f14f4f9cfa0
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2021-11-11 15:59:51 -0300

    Model ran for the first time

--- renshuu/renshuu.py
+++ renshuu/renshuu.py
@@ -72,8 +72,24 @@ random.shuffle(training)
 training = numpy.array(training)
 
 train_x = list(training[:, 0])
-train_x = list(training[:, 1])
+train_y = list(training[:, 1])
 
 
+"""
+Construindo o módulo da rede neural (NEURAL NETWORK THEORY):
+"""
 
-print("Funciona até aqui (COMPILA NO CASO HAHAHAHAHA :p")
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(numpy.array(train_x), numpy.array(train_y), epochs=200, batch_size = 5, verbose = 1)
+model.save('nagon_model.model')
+print("Done")
