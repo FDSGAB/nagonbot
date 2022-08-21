f62be416beacd4969a3d8fba5df3b71f477d1b12
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2022-08-20 23:26:27 -0300

    Working on dictionary function

---
+++ functions/dictionary_bs4.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+search_term = "木"
+url = "https://www.weblio.jp/content/" + search_term
+
+
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find_all(class_="kijiWrp") #Results é uma lista com todos os artigos relacionados à palavra do website. Definição, dicionário de kanji etc.
+
+
+
+titles = soup.find_all(class_ = "midashigo")
+for titl in titles:
+    print(titl.prettify())
+descriptions = soup.find_all(class_ = "Sgkdj")
+number_list = list()
+for description in descriptions:
+    numbers = description.find_all("b")
+    if not numbers:  #verifica se a lista esta vazia ou nao
+        number_list.append(1)
+        continue
+    for number in numbers:
+        try:
+            number_list.append(int(number.text))
+        except:
+            continue
+#print(number_list)
+
+#print(len(number_list))
+
+for i in range (0,len(number_list)):
+    try:
+        prt_titl = titles[i]
+        bibi = prt_titl.find("sup")
+        print(bibi.text())
+    except:
+        continue
+
+
+
+
+
+
--- functions/dictionary_selenium+bs4.py
+++ functions/dictionary_selenium+bs4.py
@@ -2,25 +2,39 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pandas as pd
+import requests
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-URL = "https://www.weblio.jp/"
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+url = "https://www.weblio.jp/"
 search_term = "木"
 
-driver.get(URL)
+
+
+"""
+Abre o driver do Chromium (instala se for necessário), 
+passa o URL pra ele, 
+procura a barra de pesquisa,
+insere a palavra na caixa de pesquisa,
+procura a palavra,
+pega o url da página da palavra procurada,
+fecha o driver.
+"""
+
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+driver.get(url)
 search_bar = driver.find_element(By.NAME, "query")
 search_bar.send_keys(search_term)
 search_bar.send_keys(Keys.ENTER)
-
-while True:
-    m = int(input())
-    if m ==1:
-        break
-
-#Fecha o brouser
+word_page_url = requests.get(driver.current_url)
 driver.close()
+
+
+#soup = BeautifulSoup(word_page_url.text, 'html.parser')
+soup = BeautifulSoup(word_page_url.content, "html.parser")
+
+print(soup.prettify())
+
 
 
 #soup = BeautifulSoup(page.content, "html.parser")
