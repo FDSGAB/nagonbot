89a0fe4802ce89bfc9ac775fca42740355e12f1e
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2022-12-03 18:03:16 -0300

    Main adjustments

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from functions.voice import Voice
 from kokoro.kokorowoyomu import Json_Reader
 from model import Model
 
-
+#pre-setup
 json_file = Json_Reader()
 model = Model()
 sentence = Sentence()
 os.system('cls')
 koe = Voice()
 
-
-
 #Loop principal para o funcionamento do chatbot com opção de desligá-lo　se a tag for de despedida
-while True:
-    message = input("\n自分:\n")
-    ints = sentence.predict_class(message, model.model, model.classes, json_file.tagger, model.words)
-    res = sentence.get_response(koe,ints, json_file.kokoro)
-    koe.voice_answer(res)
-    if ints[0]['koko']  in ["さようなら" , "寝るさようなら"]:
-        break
+def main():
+    while True:
+        message = input("\n自分:\n")
+        ints = sentence.predict_class(message, model.model, model.classes, json_file.tagger, model.words)
+        response = sentence.get_response(koe, ints, json_file.kokoro)
+        koe.voice_answer(response)
+        if ints[0]['koko']  in ["さようなら" , "寝るさようなら"]:
+            break
+
+if __name__ == '__main__':
+    main() 
