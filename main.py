2dd718c2022fc873321ac7e0f81e59b308ac6663
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2023-01-25 19:35:41 -0300

    BGM function added (still needs improvments)
    The bgm functions work, but not on a loop. The issue needs to be fixed

--- main.py
+++ main.py
@@ -5,26 +5,41 @@ from functions.voice import Voice
 from kokoro.json_reader import Json_Reader
 from model import Model
 import logging
-
-
-#pre-setup
-os.environ['WDM_LOG'] = "false"
-logging.getLogger('WDM').setLevel(logging.NOTSET)
-json_file = Json_Reader()
-model = Model()
-sentence = Sentence()
-os.system('cls')
-voice = Voice()
+from sound import BGM
 
 #Loop principal para o funcionamento do chatbot com opção de desligá-lo　se a tag for de despedida
 def main():
+
+    #pre-setup
+    os.environ['WDM_LOG'] = "false"
+    logging.getLogger('WDM').setLevel(logging.NOTSET)
+    json_file = Json_Reader()
+    model = Model()
+    sentence = Sentence()
+    os.system('cls')
+    voice = Voice()
+    bgm_switch = False
+    bgm_is_playing = False
+
     while True:
+        if bgm_is_playing == False:
+            BGM().play_song(bgm_switch)
+            if bgm_switch:
+                bgm_is_playing = True
         message = input("\n自分:\n")
         ints = sentence.predict_class(message, model.model, model.classes, json_file.tagger, model.words)
         response = sentence.get_response(voice, ints, json_file.kokoro)
         voice.voice_answer(response)
         if ints[0]['koko']  in ["さようなら" , "寝るさようなら"]:
             break
+        if ints[0]['koko'] == "BGM_ON":
+            if not bgm_switch:
+                bgm_switch = True
+        if ints[0]['koko'] == "BGM_OFF":
+            if bgm_switch:
+                bgm_switch = False
+                if bgm_is_playing:
+                    bgm_is_playing = False
 
 if __name__ == '__main__':
     main() 
---
+++ sound/bgm.py
@@ -0,0 +1,56 @@
+import random
+import pygame
+
+class BGM():
+
+    bgm_list = ["./sound/monstruo.wav", "./sound/new_spring.wav", "./sound/お雑煮の香り.wav", "./sound/竜宮城.wav", "./sound/低音忠実性.wav"]
+    current_song = "./sound/お雑煮の香り.wav"
+    bgm_switch = False
+
+    def start(self):
+        self.pick_song()
+    
+    def pick_song(self):
+        picked_random_number = random.randint(0,len(self.bgm_list)-1)
+        song_name = self.bgm_list[picked_random_number]
+        print("\n曲:", song_name[8:len(song_name)-4])
+        self.current_song = self.bgm_list[picked_random_number]
+
+
+    def play_song(self, switch: bool):
+        if switch == True and self.bgm_switch == True:
+            return
+        self.bgm_switch = switch
+        if self.bgm_switch:
+            self.pick_song()
+            pygame.init()
+            pygame.mixer.init(44100,-16,2,512)
+            sound = pygame.mixer.Sound(self.current_song)
+            sound.set_volume(0.034)
+            sound.play()
+        else:
+            pygame.init()
+            pygame.mixer.init(44100,-16,2,512)
+            sound = pygame.mixer.Sound(self.current_song)
+            sound.set_volume(0.034)
+            sound = pygame.mixer.pause()
+
+
+if __name__ == "__main__":
+    bgm_switch = False
+
+    while True:
+        BGM().play_song(bgm_switch)
+        switch = input("Turn on switch? (y/n) \n")
+        if switch == "y":
+            bgm_switch = True
+        else:
+            bgm_switch = False
+    
+
+
+
+
+
+
+
