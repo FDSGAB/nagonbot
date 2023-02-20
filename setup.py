12373faff59d5ab876a6e69876a7e94b75aa12e0
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2023-02-20 15:19:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 #py setup.py sdist bdist_wheel
+#py -m twine upload --repository testpypi dist/*
 
 from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 DESCRIPTION = 'Japanese Chatbot'
 
-with open('./LICENSE.md', 'r') as license_md:
+with open('./LICENSE.md', encoding = 'utf8') as license_md:
     license_file = license_md.read()
     
 
-with open('./README.md', 'r') as readme:
+with open('./README.md', encoding = 'utf8') as readme:
     LONG_DESCRIPTION = readme.read()
 
-# Setting up
-with open('./requirements.txt','r') as requirements_text:
+with open('./requirements.txt', encoding = 'utf8') as requirements_text:
     required = requirements_text.read().split("\n")
 
 setup(
     name="nagonbot",
     version=VERSION,
     author="Gabriel Fioravante Di Sciascio (FDSGAB)",
+    maintainer="Gabriel Fioravante Di Sciascio (FDSGAB)",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
@@ -29,11 +30,15 @@ setup(
     license = license_file,
     packages = find_packages(exclude=('tests','docs')),
     install_requires = required,
-    keywords=['python', 'chatbot', 'japanese', 'nagon', 'bot'],
+    keywords=['python', 'chatbot', 'japanese', 'nagon', 'bot', 'artificial intelligence', 'virtual assistant'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
+        'Environment :: Console',
+        "License :: OSI Approved :: MIT License",
+        'Intended Audience :: End Users/Desktop',
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    python_requires='>=3.7',
 )
