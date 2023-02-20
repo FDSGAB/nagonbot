cab49b04c2cb5cb4a103c1b7e4191825a45e2d6a
Gabriel Fioravante Di Sciascio <kirkcaulfieldb@gmail.com>
2023-02-20 16:16:42 -0300

    Delete outer.py

--- outer.py
+++
@@ -1,7 +0,0 @@
-with open('./requirements.txt','r') as requirements_text:
-    required = requirements_text.read().split("\n")
-print(required)
-
-for element in required:
-    
-    print(isinstance(element,str),":" ,element)
